5a1e742621e87a0fb2a99ad295f75ff52087384f
Astro <astro@spaceboyz.net>
2024-08-17 02:12:51 +0200

    descent3: update to v1.5.0, cleanup

--- pkgs/by-name/de/descent3/package.nix
+++ pkgs/by-name/de/descent3/package.nix
@@ -1,19 +1,18 @@
 { lib, stdenv, fetchFromGitHub
 , cmake, ninja
 , ncurses, SDL2, SDL2_image, zlib
-# , pkgsi686Linux
 }:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   pname = "descent3";
-  version = "unstable-2024-04-28";
+  version = "1.5.0";
 
   _src = /home/stephan/programs/descent3;
   src = fetchFromGitHub {
     owner = "DescentDevelopers";
     repo = "Descent3";
-    rev = "fd615490803d08f46a407ce697e741f697519125";
-    hash = "sha256-6tjWfsJE/L+wS9/JBUnGNZ7vyITyEr9IvZbVtjkT6yc=";
+    rev = "v${version}";
+    hash = "sha256-IjKtnAf0IGblZ7yd3qB0GVTQ3QZZ4lXpsCT/tl3fW4Y=";
   };
 
   _postPatch = ''
@@ -29,26 +28,14 @@ stdenv.mkDerivation {
 
   buildInputs = [ ncurses SDL2 SDL2_image zlib ];
 
-  # LD_LIBRARY_PATH = lib.concatMapStringsSep ":" (lib: "${lib}/lib") (
-  #   with pkgsi686Linux.xorg; [
-  #     libX11.out libXext.out
-  #   ]);
-
   HOME = "${placeholder "out"}/share";
 
-  cmakeFlags = [ "-DD3_GAMEDIR=Descent3" ];
-  #cmakeBuildType="Debug";
-  # ninjaFlags = [ "Descent3" ];
+  cmakeFlags = [ "-DENABLE_LOGGER=ON" "-DD3_GAMEDIR=Descent3" ];
+  cmakeBuildType="Debug";
 
   # error: boost::bad_format_string: format-string is ill-formed
   hardeningDisable = [ "format" ];
 
-  # preBuild = ''
-  #   pwd
-  #   ls -la ../scripts/hogutils
-  #   ${pkgsi686Linux.glibc.bin}/bin/ldd ../scripts/hogutils/hogUtils-i686
-  #   ../scripts/hogutils/hogUtils-i686 --help
-  # '';
   installPhase = ''
     mkdir -p $out/bin
     cp Descent3/Descent3 $out/bin/
